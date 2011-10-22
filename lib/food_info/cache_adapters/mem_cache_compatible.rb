56aef1d58da4cd999d2289558108ee9cde9971a8
Kali Donovan <kali@deviantech.com>
2011-10-22 14:18:40 +0200

    Cache should return value after storing

--- lib/food_info/cache_adapters/mem_cache_compatible.rb
+++ lib/food_info/cache_adapters/mem_cache_compatible.rb
@@ -11,7 +11,12 @@ module FoodInfo
       end
       
       def set(key, val)
-        @cache.set(key, val)
+        begin
+          @cache.set(key, val)
+        rescue Exception => e
+          STDERR.puts e
+        end
+        val
       end
 
       def get(key)
